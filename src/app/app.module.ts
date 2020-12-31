ed232afad5a8654442c92212b1020e82d9837073
Joko Wijaya <jokowi@gmail.com>
2020-12-30 21:44:37 -0800

    establish basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminCalendarComponent } from './admin-calendar/admin-calendar.component';
+import { AdminSettingComponent } from './admin-setting/admin-setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    AdminCalendarComponent,
+    AdminSettingComponent
   ],
   imports: [
     BrowserModule,
