70ed6bbe6226ad11a76d916cb9afffd58932c8e5
Joko Wijaya <jokowi@gmail.com>
2021-01-10 01:56:05 -0800

    implement calendar dates

--- src/app/admin-calendar/admin-calendar.component.ts
+++ src/app/admin-calendar/admin-calendar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-admin-calendar',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminCalendarComponent implements OnInit {
 
-  constructor() { }
+  date:Date;
+  yearStart:number;
+  yearSelected:Number;
+  monthSelected:Number;
+  daySelected:Number;
+  dayRange: Array<Number>;
+  yearRange: Array<Number>;
+  
+  constructor(private router: Router) { 
+    this.date = new Date();
+    this.yearStart = this.date.getFullYear()-3;
+    this.yearRange = [...Array(12).keys()].map(i => i + this.yearStart);
+    this.yearSelected = this.date.getFullYear();
+    this.monthSelected = this.date.getMonth()+1;
+    this.daySelected = this.date.getDate();
+    this.dayRange = [...Array(31).keys()].map(i => i + 1);
+  }
 
   ngOnInit(): void {
+    this.router.navigate([], {queryParams:{date:this.date.toLocaleDateString("en-US")}});
   }
 
 }
