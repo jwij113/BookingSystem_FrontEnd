f4514c7e17b2752655030e9a0cc75e49dd5b9e0c
Joko Wijaya <jokowi@gmail.com>
2021-01-11 20:20:48 -0800

    implement calendar dates - finished

--- src/app/admin-calendar/admin-calendar.component.ts
+++ src/app/admin-calendar/admin-calendar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-admin-calendar',
@@ -8,26 +9,58 @@ import {Router} from "@angular/router";
 })
 export class AdminCalendarComponent implements OnInit {
 
-  date:Date;
+  currentDate:Date;
   yearStart:number;
-  yearSelected:Number;
-  monthSelected:Number;
-  daySelected:Number;
-  dayRange: Array<Number>;
-  yearRange: Array<Number>;
+  yearSelected:number = 0;
+  monthSelected:number = 0;
+  daySelected:number = 0;
+  dayRange: Array<number>;
+  yearRange: Array<number>;
+  monthRange: Array<string>;
+  dateParam: string = "";
   
-  constructor(private router: Router) { 
-    this.date = new Date();
-    this.yearStart = this.date.getFullYear()-3;
+  constructor() { 
+    
+    this.currentDate = new Date();
+    this.yearStart = this.currentDate.getFullYear()-3;
+
     this.yearRange = [...Array(12).keys()].map(i => i + this.yearStart);
-    this.yearSelected = this.date.getFullYear();
-    this.monthSelected = this.date.getMonth()+1;
-    this.daySelected = this.date.getDate();
     this.dayRange = [...Array(31).keys()].map(i => i + 1);
+    this.monthRange = ["January", "February", "March",
+                       "April", "May", "June", "July",
+                       "August", "September", "October",
+                       "November", "December"
+                      ]
+
+    this.yearSelected = this.currentDate.getFullYear();
+    this.monthSelected = this.currentDate.getMonth()+1;
+    this.daySelected = this.currentDate.getDate();    
+
   }
 
-  ngOnInit(): void {
-    this.router.navigate([], {queryParams:{date:this.date.toLocaleDateString("en-US")}});
+  ngOnInit(): void { 
   }
 
+  changeYear(year:number){
+    var datesArr:Array<string> = this.dateParam.split("/");
+    datesArr[2] = year.toString();
+    this.yearSelected = parseInt(datesArr[2]);
+  }
+
+  changeMonth(month:number){
+    var datesArr:Array<string> = this.dateParam.split("/");
+    datesArr[1] = month.toString();
+    this.monthSelected = parseInt(datesArr[1]);
+  }
+
+  changeDay(day:number){
+    var datesArr:Array<string> = this.dateParam.split("/");
+    datesArr[0] = day.toString();
+    this.daySelected = parseInt(datesArr[0]);
+  }
+
+  changeCalendarWeek(){
+    var date:String = this.daySelected+"/" + this.monthSelected + "/"+ this.yearSelected;
+    console.log(date);
+  }
 }
